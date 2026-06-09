04bc42de166ccdb031ec791c0d64b1c23cacdae7
Ömer Özata <omerozata015@gmail.com>
2026-06-09 22:00:00 +0300

    OKHttp added and TypeConverter added for room

--- app/src/main/java/com/omero/yeni/di/NetworkModule.kt
+++ app/src/main/java/com/omero/yeni/di/NetworkModule.kt
@@ -5,6 +5,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -14,12 +16,35 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
+    // 1. Adım: Ağ trafiğini dinleyecek olan Ajanı (Interceptor) oluşturuyoruz
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideLoggingInterceptor() : HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            // Level.BODY: Gönderilen isteklerin ve gelen cevapların (JSON dahil) her şeyini loglar.
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    // 2. Adım: Retrofit'in arkasında çalışan asıl motoru (OkHttpClient) kuruyoruz
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor) : OkHttpClient {
+        return OkHttpClient.Builder()
+        // Az önce oluşturduğumuz ajanı motorun içine entegre ediyoruz
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    // 3. Adım: Retrofit nesnesini oluştururken yukarıdaki motoru içine veriyoruz
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://rickandmortyapi.com/api/") // Rick and Morty'nin ana adresi
             .addConverterFactory(GsonConverterFactory.create()) //Gelen JSON'ı Kotlin objesine çevir
+            // Profesyonel dokunuş: Standart motor yerine kendi özelleştirilmiş motorumuzu bağladık
+            .client(okHttpClient)
             .build()
     }
 
