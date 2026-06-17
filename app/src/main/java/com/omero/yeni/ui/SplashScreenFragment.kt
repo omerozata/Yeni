1df047544ef0a89e22515dd7ccca241a75fede66
Ömer Özata <omerozata015@gmail.com>
2026-06-17 15:58:17 +0300

    feat(navigation): add splash screen as initial destination

---
+++ app/src/main/java/com/omero/yeni/ui/SplashScreenFragment.kt
@@ -0,0 +1,13 @@
+package com.omero.yeni.ui
+
+
+import androidx.fragment.app.Fragment
+import com.omero.yeni.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashScreenFragment : Fragment(R.layout.fragment_splash_screen) {
+
+
+
+}
