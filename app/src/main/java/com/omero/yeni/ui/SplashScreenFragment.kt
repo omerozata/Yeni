f99ac5bba5d6157a8de8f9fc97fcbfccc514a9ae
Ömer Özata <omerozata015@gmail.com>
2026-06-17 18:08:44 +0300

    😊 SplahsScreen sayfasına gülücük eklendi | Artık daha profesyonel :)

--- app/src/main/java/com/omero/yeni/ui/SplashScreenFragment.kt
+++ app/src/main/java/com/omero/yeni/ui/SplashScreenFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import com.omero.yeni.R
 import dagger.hilt.android.AndroidEntryPoint
 
+// :)
+
 @AndroidEntryPoint
 class SplashScreenFragment : Fragment(R.layout.fragment_splash_screen) {
 
