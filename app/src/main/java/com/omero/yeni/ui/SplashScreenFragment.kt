1ea42cd65805b1ac94d69c0a660126324bfbd379
Ömer Özata <omerozata015@gmail.com>
2026-08-02 18:57:21 +0300

    🚧 Açılış mimarisini komple değiştiriyoruz | Splash sadece bekleme salonu olacak

--- app/src/main/java/com/omero/yeni/ui/SplashScreenFragment.kt
+++ app/src/main/java/com/omero/yeni/ui/SplashScreenFragment.kt
@@ -4,14 +4,12 @@ package com.omero.yeni.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.omero.yeni.R
-import com.omero.yeni.vievmodel.SplashScreenViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withTimeoutOrNull
 import kotlin.time.Duration.Companion.milliseconds
 
 // :)
@@ -19,17 +17,15 @@ import kotlin.time.Duration.Companion.milliseconds
 @AndroidEntryPoint
 class SplashScreenFragment : Fragment(R.layout.fragment_splash_screen) {
 
-private val viewModel: SplashScreenViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewLifecycleOwner.lifecycleScope.launch {
-            // Splash ekranında veriyi hazırlamak için en fazla 3 saniye veriyoruz
-            withTimeoutOrNull(3000.milliseconds) {
-                viewModel.prepareAppConfiguration()
-            }
-            // Süre dolduğunda veya veri geldiğinde Home ekranına zıpla
+            // Görsellik ve MainViewModel'daki isteğe avans vermek için 2 saniye bekle
+            delay(2000.milliseconds)
+
+            // Home ekranına geç
             findNavController().navigate(R.id.action_splashScreenFragment_to_homeFragment)
         }
     }
